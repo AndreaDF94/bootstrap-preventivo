7f4c7318396cb72a105ff1382e43d39a7dc8c28b
Andrea DF94 <defilippiandre@libero.it>
2025-06-07 12:17:24 +0200

    Creo oggetto lavori con descrizione e prezzi orari per ogni tipo di servizio

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ const form = document.getElementById("quote-form");
 const select = document.getElementById("job-type");
 const promoInput = document.getElementById("promo");
 const prezzoFinale = document.getElementById("prezzo");
+// Oggetto con opzioni di lavoro e relativi prezzi orari
+const lavori = {
+  backend: { nome: "Backend Development", prezzo: 20.5 },
+  frontend: { nome: "Frontend Development", prezzo: 15.3 },
+  analysis: { nome: "Project Analysis", prezzo: 33.6 },
+};
