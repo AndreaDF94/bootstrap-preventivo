cccc1418e1bcd48cf503ed2ff4968c07134f8485
Andrea DF94 <defilippiandre@libero.it>
2025-06-07 12:15:20 +0200

    Seleziono elementi del DOM: form,select,campo promo,campo prezzo finale

---
+++ script.js
@@ -0,0 +1,5 @@
+// Otteniamo riferimenti agli elementi del DOM
+const form = document.getElementById("quote-form");
+const select = document.getElementById("job-type");
+const promoInput = document.getElementById("promo");
+const prezzoFinale = document.getElementById("prezzo");
